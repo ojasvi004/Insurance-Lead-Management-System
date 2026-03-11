d7f494311c9c745bff4248abb4f8004bffa583e1
Ojasvi <ojasvidoye@gmail.com>
2026-03-11 11:58:09 +0530

    add show lead details method

--- src/application/MainApplication.java
+++ src/application/MainApplication.java
@@ -32,6 +32,10 @@ public class MainApplication {
 				registerNewLead(sc);
 			} else if (choice.equals("2")) {
 				editLeadDetails(sc);
+			}else if (choice.equals("3")) {
+				deleteLeadRecord(sc);
+			}else if (choice.equals("4")) {
+				showLeadDetails(sc);
 			} else {
 				System.out.println("Invalid option.");
 			}
@@ -232,5 +236,41 @@ public class MainApplication {
 		}
 		DataObjects.DataSource.partyData.remove(leadSeq);
 		System.out.println("Lead deleted successfully!");
+	}
+	
+	static void showLeadDetails(Scanner sc) {
+		System.out.println("Enter the details of the lead ID you want to see: ");
+		String leadSeq = sc.nextLine();
+		
+		if(!DataObjects.DataSource.partyData.containsKey(leadSeq)) {
+			System.out.println("Lead ID not found. Enter correct Lead ID");
+			return;
+		}
+		DataObjects.LeadDO leadInfo = DataObjects.DataSource.partyData.get(leadSeq);
+		System.out.println("Lead ID: " + leadInfo.getLeadSeq());
+		System.out.println("Name: " + leadInfo.getTitle() + " " + leadInfo.getFirstName());
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Date of Birth: " + sdf.format(leadInfo.getBirthDate()));
+		System.out.println("Gender: " + leadInfo.getGenderCd());
+		System.out.println("Addresses");
+		List<DataObjects.LeadAddressDO> addresses = leadInfo.getLeadAddressDOList();
+		if (addresses.size() > 0) {
+			for (int i = 0; i <addresses.size(); i++) {
+				DataObjects.LeadAddressDO addr = addresses.get(i);
+				System.out.println((i + 1) + ": " +addr.getAddressType() + " - " + addr.getPinCode());
+			}
+		} else {
+			System.out.println("No addresses found.");
+		}
+		System.out.println("Contacts");
+		List<DataObjects.LeadContactDO> contacts = leadInfo.getLeadContactDOList();
+		if (contacts.size() > 0) {
+			for (int i = 0; i<contacts.size(); i++) {
+				DataObjects.LeadContactDO contact = contacts.get(i);
+				System.out.println((i+1)+": "+contact.getContactType() + " - "+contact.getContactNum());
+			}
+		} else {
+			System.out.println("No contacts found");
+		}
 	}
 }
